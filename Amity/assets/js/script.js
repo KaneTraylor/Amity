1ccf42eed183bd240db9b7b4529e9d58866c20e6
Atum131 <121517251+Atum131@users.noreply.github.com>
2023-03-17 15:59:38 -0400

    bunch of nuts

--- Amity/assets/js/script.js
+++ Amity/assets/js/script.js
@@ -11,3 +11,13 @@ const randomImageUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)];
 
 // Set the background image of the section element to the random image URL
 document.getElementById("random-image-section").style.backgroundImage = `url(${randomImageUrl})`;
+
+<script>
+        // Set the scroll interval to 3 seconds
+    setInterval(function () {
+            // Get the wrapper div
+            var wrapper = document.querySelector('.wrapper');
+    // Scroll the wrapper to the right by the width of one listing box
+    wrapper.scrollBy({left: 280, behavior: 'smooth' });
+        }, 3000);
+</script>
