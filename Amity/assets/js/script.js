f2ce13e2acb39ebaf61f719c773428c0087abfcc
KaneTraylor <kanetraylor556@gmail.com>
2023-03-17 14:59:31 -0400

    index changes

--- Amity/assets/js/script.js
+++ Amity/assets/js/script.js
@@ -1,9 +1,9 @@
 // Define an array of image URLs
 const imageUrls = [
-    "https://images.unsplash.com/photo-1495660611116-fdd372baad4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-    "https://images.unsplash.com/photo-1660638986113-c6da7ff96cca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    "https://images.unsplash.com/photo-1621293051751-6514b7f9515d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1156&q=80",
+    "https://images.unsplash.com/photo-1641697633078-d756dac7dea7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
     "https://images.unsplash.com/photo-1648221231735-d2da7afed3b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1168&q=80",
+    "https://images.unsplash.com/photo-1527525443983-6e60c75fff46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1970&q=80",
+    "https://images.unsplash.com/photo-1648221231877-0eec638caf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1168&q=80",
 ];
 
 // Get a random image URL from the array
